f0f5c31353b590fecca3f760991dacad015b572b
Fadhel Faturrahman <fadhelfaturrahman@gmail.com>
2019-09-01 13:36:42 +0700

    Updates

---
+++ precache-manifest.0fd418026b1e123cb336b9c961f2a32b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228f6f0e714bb3450f699fa3c264f625",
+    "url": "/workforce/index.html"
+  },
+  {
+    "revision": "710da93cd61a22ab6573",
+    "url": "/workforce/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8338010131259fd38bc5",
+    "url": "/workforce/static/js/2.d9edae29.chunk.js"
+  },
+  {
+    "revision": "710da93cd61a22ab6573",
+    "url": "/workforce/static/js/main.cc2080a8.chunk.js"
+  },
+  {
+    "revision": "6b64ad6b772b19f0cc00",
+    "url": "/workforce/static/js/runtime~main.196266ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workforce/static/media/logo.5d5d9eef.svg"
+  }
+]);
